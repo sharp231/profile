77198ff340f11b3a0a23181dc59b7f821cd011aa
SimBa296 <yuudai07ya@gmail.com>
2022-09-03 00:03:18 +0900

    js　編集

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 
-(function(){
+(function() {
     setTimeout(index01, 10);
-      function index01(){
+      function index01() {
         var scrollElemToWatch_1 = document.getElementById('index01')
             watcher_1 = scrollMonitor.create(scrollElemToWatch_1, -10), 
             rev1 = new RevealFx(scrollElemToWatch_1, {
                 revealSettings : {
                     bgcolor :'#FFCC33',
                     duration: 300, 
-                    direction:'rl',
+                    // direction:'rl',
+
                     onStart:function(contentEl, revealerEl){
                         anime.remove(contentEl);
                         contentEl.style.opacity = 0;
@@ -25,7 +26,7 @@
                     }
                 }
             })
-            watcher_1.entterViewport(function() {
+            watcher_1.enterViewport(function() {
                 rev1.reveal();
                     watcher_1.destroy();
             });
