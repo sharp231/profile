de91247b5a765bb2f59217bb0d3acd4bc4c800fb
SimBa296 <yuudai07ya@gmail.com>
2022-09-06 16:54:15 +0900

    キータに質問中

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 
 (function() {
-    setTimeout(init, 10);
-      function init() {
-        var scrollElemToWatch_1 = document.getElementById('index01') //指定
-            watcher_1 = scrollMonitor.create(scrollElemToWatch_1, -300), 
+    setTimeout(index01, 10);
+      function index01() {
+
+        let scrollElemToWatch_1 = document.getElementById('index01') //指定
+            watcher_1 = scrollMonitor.create(scrollElemToWatch_1, -10), 
             rev1 = new RevealFx(scrollElemToWatch_1, {
                 revealSettings : {
                     bgcolor :'#FFCC33',
                     duration: 300, 
                     // direction:'rl',
 
-                    onStart:function(contentEl, revealerEl){
+                    onStart: function(contentEl, revealerEl){
                         anime.remove(contentEl);
                         contentEl.style.opacity = 0;
                     },
@@ -18,10 +19,10 @@
                         contentEl.style.opacity = 1;
                         anime ({
                             targets : contentEl,
-                            duration : 100, 
-                            delay:10, 
+                            // duration : 100, 
+                            // delay:10, 
                             easing: 'easeOutExpo',
-                            opacity:[0,1]
+                            // opacity:[0,1]
                         });
                     }
                 }
